30ac6474def483f946d41e04353b6545d1b83f7f
raj-shap <lathigararaj0@gmail.com>
2025-02-05 12:18:36 +0530

    update register view

--- InventoryManagement/Models/User.cs
+++ InventoryManagement/Models/User.cs
@@ -5,7 +5,7 @@
         public string id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string UserId { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
--- InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f807c98fd6e1db6fd39abab57f2373b4ca70aff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c8dc4038dafcf602a0fe326e3b3d2b079c2257")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
